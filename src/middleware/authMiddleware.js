b6355110cc8da6d69fe9065d7ef65109d3a312e1
SirYazoo <rioaurelio25@gmail.com>
2026-03-04 20:49:58 +0700

    refactor: use optional chaining for auth header check as per SonarQube recommendation

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,10 +4,7 @@ require("dotenv").config();
 const protect = async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
+  if (req.headers.authorization?.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
 
