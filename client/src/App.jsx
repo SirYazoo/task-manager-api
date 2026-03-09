3e4e360d844ddea6ae70de2c65bdb974eb99782d
SirYazoo <rioaurelio25@gmail.com>
2026-03-09 15:52:47 +0700

    fix: resolve case-sensitive import error for Register component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 
 function App() {
   return (
