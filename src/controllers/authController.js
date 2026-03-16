09a8d0b380dad3fb307cdbac3e122a9816f1e2c5
SirYazoo <rioaurelio25@gmail.com>
2026-03-16 12:22:34 +0700

    fix(auth): update duplicate email response to 409 Conflict

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ const register = async (req, res) => {
       email,
     ]);
     if (userExists.rows.length > 0) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(409).json({ message: "User already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
