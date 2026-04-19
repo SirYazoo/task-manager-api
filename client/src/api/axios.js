001a657c8c3c43d943c36fb73677648fc1fc7690
SirYazoo <rioaurelio25@gmail.com>
2026-04-19 23:14:46 +0700

    fix: handle expired JWTs with global axios interceptor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -17,4 +17,18 @@ api.interceptors.request.use(
   },
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      globalThis.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default api;
